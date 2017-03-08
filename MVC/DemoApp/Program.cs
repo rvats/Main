219b3cacd86c8a2843a953249e31d9f8710c86f8
Rahul Vats <vats.rahul@gmail.com>
2017-03-08 12:20:45 -0600

    Adding Libraries to dig deeper into programming

---
+++ MVC/DemoApp/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using DemoApp;
+
+namespace ChatTutorial.ConsoleServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var server = new ChatServer();
+                server.Start();
+                Console.WriteLine("Listening....");
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Logger.PrintException(ex, "Main");
+            }
+        }
+    }
+
+    internal class ChatServer
+    {
+        private readonly TcpListener _listener = new TcpListener(IPAddress.Parse("0.0.0.0"), 7776);
+        private readonly Dictionary<string, LocalClient> _clients = new Dictionary<string, LocalClient>();
+
+        public async void Start()
+        {
+            try
+            {
+                _listener.Start();
+                while (true)
+                {
+                    try
+                    {
+                        Console.WriteLine("Listening...");
+                        var client = await _listener.AcceptTcpClientAsync();
+
+                        Console.WriteLine("Client connected");
+                        var id = Guid.NewGuid().ToString();
+                        _clients.Add(id, new LocalClient(client, ReceivedMessage, ClientClosed, id));
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Start() While() '{0}', '{1}'", ex.Message, ex.StackTrace);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.PrintException(ex, "Start");
+            }
+        }
+
+        public void ClientClosed(string id)
+        {
+            Console.WriteLine("Client {0} disconnected", id);
+            var client = _clients[id];
+            _clients.Remove(id);
+            client.Close();
+            Console.WriteLine("Client {0} Closed", id);
+            ReceivedMessage("Client disconnected", id);
+        }
+
+        public void ReceivedMessage(string message, string id)
+        {
+            try
+            {
+                var clientsToRemove = new List<string>();
+                foreach (var client in _clients)
+                {
+                    if (client.Value != null && client.Value.Client.Connected)
+                    {
+                        if (client.Key != id)
+                            client.Value.Send(id + "> " + message);
+                    }
+                    else
+                        clientsToRemove.Add(client.Key);
+                }
+
+                foreach (var client in clientsToRemove)
+                    _clients.Remove(client);
+            }
+            catch (Exception ex)
+            {
+                Logger.PrintException(ex, "ReceivedMessage");
+            }
+        }
+    }
+
+    public static class Logger
+    {
+        public static void PrintException(Exception ex, string methodName)
+        {
+            Console.WriteLine("{0}: \n '{1}' \n '{2}'", methodName, ex.Message, ex.StackTrace);
+        }
+    }
+}
---
+++ MVC/ServerSocket1/ServerSocket1.cs
@@ -0,0 +1,67 @@
+﻿// created on 9/21/2001 at 10:59 AM
+using System;
+using System.Net.Sockets;
+using System.IO;
+using System.Net;
+
+public class ServerSocket1
+{
+    public static void Main()
+    {
+        try
+        {
+            bool status = true;
+            string servermessage = "";
+            string clientmessage = "";
+            TcpListener tcpListener = new TcpListener(IPAddress.Parse("0.0.0.0"), 8100);
+            tcpListener.Start();
+            Console.WriteLine("Server Started");
+
+            Socket socketForClient = tcpListener.AcceptSocket();
+            Console.WriteLine("Client Connected");
+            NetworkStream networkStream = new NetworkStream(socketForClient);
+            StreamWriter streamwriter = new StreamWriter(networkStream);
+            StreamReader streamreader = new StreamReader(networkStream);
+
+            while (status)
+            {
+                if (socketForClient.Connected)
+                {
+
+
+
+                    servermessage = streamreader.ReadLine();
+                    Console.WriteLine("Client:" + servermessage);
+                    if ((servermessage == "bye"))
+                    {
+                        status = false;
+                        streamreader.Close();
+                        networkStream.Close();
+                        streamwriter.Close();
+                        return;
+
+                    }
+                    Console.Write("Server:");
+                    clientmessage = Console.ReadLine();
+
+                    streamwriter.WriteLine(clientmessage);
+                    streamwriter.Flush();
+
+
+
+                }
+
+            }
+            streamreader.Close();
+            networkStream.Close();
+            streamwriter.Close();
+            socketForClient.Close();
+            Console.WriteLine("Exiting");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+
+    }
+}
