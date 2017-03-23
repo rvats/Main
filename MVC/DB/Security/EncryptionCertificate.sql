7d4b1e96d1da005ee58217f2da3ecd79081276b2
rvats <vats.rahul@gmail.com>
2017-03-22 20:32:57 -0500

    Test

--- MVC/DB/Security/EncryptionCertificate.sql
+++ MVC/DB/Security/EncryptionCertificate.sql
@@ -1,3 +1,39 @@
-﻿CREATE CERTIFICATE [EncryptionCertificate]
+﻿-- Creating a Master Key
+CREATE MASTER KEY ENCRYPTION BY PASSWORD = '!AimTWI030126!';
+GO
+
+-- We need to create a self-signed certificate
+CREATE CERTIFICATE [EncryptionCertificateTWI] 
+WITH SUBJECT = 'EncryptionTWI'
+GO
+
+-- Creating an Encryption Certificate with a separate key
+CREATE CERTIFICATE [EncryptionCertificateWithKey]
 	ENCRYPTION BY PASSWORD = 'aombqkh>rjDz|vkbuvukguowmsFT7_&#$!~<`oir1Ciysn_E'
-	WITH SUBJECT = 'certificate_subject'
+	WITH SUBJECT = 'EncryptionTWI'
+GO
+
+-- Create a symmetric key
+CREATE SYMMETRIC KEY SymmetricTWIKey
+WITH ALGORITHM = AES_192
+ENCRYPTION BY CERTIFICATE [EncryptionCertificateTWI]
+GO
+
+-- Usage or opening with? a key
+--OPEN SYMMETRIC KEY  SymmetricTWIKey DECRYPTION BY CERTIFICATE [EncryptionCertificateTWI]
+
+/*
+
+To encrypt: select EncryptByKey(Key_GUID('SymmetricTWIKey'), @word) as encrypted 
+To decrypt: select convert(varchar(100),DECRYPTBYKEY(@encrypted)) as decrypted  
+
+Test Script is below - 
+
+SELECT EncryptByKey(Key_GUID('SymmetricTWIKey'), 'hello')
+declare @word varchar(100), @encrypted varbinary(2000)
+set @word = 'hello'
+set @encrypted = EncryptByKey(Key_GUID('SymmetricTWIKey'), @word)
+select @encrypted
+select convert(varchar(100),DECRYPTBYKEY(@encrypted)) as decrypted  
+
+*/
---
+++ MVC/DB/Tables/Core.Users.sql
@@ -0,0 +1,48 @@
+﻿/**************************************************************************************************************************
+The database must have a MEMORY_OPTIMIZED_DATA filegroup
+before the memory optimized object can be created.
+
+The bucket count should be set to about two times the 
+maximum expected number of distinct values in the 
+index key, rounded up to the nearest power of two.
+--------------------------------------------------------------------------------------------------------------------------
+-- Table:		[Core].[Users]
+-- Author:		Rahul Vats
+-- Date:		03/15/2017
+-- Purpose:		Store the User details
+-- Notes:		This table is a generic design that stores most relevant/apprpriate user information 
+-- Depends:		n/a (or any dependencies such as other procs or functions)
+-- REVISION HISTORY ---------------------------------------------------------------------------------------------------
+-- MM/DD/YYYY	Rahul Vats	Initial creation.
+--------------------------------------------------------------------------------------------------------------------------
+**************************************************************************************************************************/
+CREATE TABLE [Core].[Users] (
+    [UserID]              INT              IDENTITY (1, 1) NOT NULL,
+    [UserName]            NVARCHAR (100)   Not NULL,
+    [Password]            NVARCHAR (255)   NULL,
+    --[IsTemporaryPassword] BIT              CONSTRAINT [DF_Users_IsTemporaryPassword] DEFAULT ((1)) NOT NULL,
+    [IsInternal]          BIT              NULL,
+    [IsActive]            BIT              CONSTRAINT [DF_Users_IsActive] DEFAULT ((1)) NOT NULL,
+    [ModifiedBy]          INT              NOT NULL,
+    [ModifiedOn]          DATETIME         DEFAULT (getutcdate()) NOT NULL,
+    [CreatedBy]           INT              DEFAULT ((0)) NOT NULL,
+    [CreatedOn]           DATETIME         DEFAULT (getutcdate()) NOT NULL,
+    [EffectiveFromDate]   DATETIME         NULL,
+	[EffectiveToDate]     DATETIME         NULL,
+    CONSTRAINT [PK_User_UserID] PRIMARY KEY CLUSTERED ([UserID] ASC)
+    --CONSTRAINT [IX_UserName] UNIQUE NONCLUSTERED ([UserName] ASC)
+) WITH (MEMORY_OPTIMIZED = ON)
+GO
+--CREATE NONCLUSTERED INDEX [IX_Users_IsActive_UserName_UserID] ON [Core].[Users]
+--(
+--	[IsActive] ASC,
+--	[UserName] ASC,
+--	[UserID] ASC
+--)
+--INCLUDE ( 	
+--	[Password],
+--	[EffectiveToDate],
+--	[ModifiedBy],
+--	[ModifiedOn]
+--) WITH (SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF) ON [PRIMARY]
+--GO
